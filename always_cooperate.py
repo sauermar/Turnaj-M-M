dc95e8c664823031d280fac8df32c18723bf4d8b
Martin Dvořák <martin.dvorak@matfyz.cz>
2020-03-06 00:20:27 +0100

    bilingual comments

--- always_cooperate.py
+++ always_cooperate.py
@@ -2,19 +2,31 @@ from player import Move, Player
 
 
 class AlwaysCooperate(Player):
-    """Hráč vždy kooperující (strategie C)."""
+    """Hráč vždy kooperující (strategie C).
+	
+	Always-cooperating player (strategy C).
+	"""
 
     def __init__(self):
-        """Nepotřebujeme si nic pamatovat."""
+        """Nepotřebujeme si nic pamatovat.
+		
+		We don't need to store anything.
+		"""
         pass
 
     def author_name(self):
         return "Marketa"
 
     def next_move(self):
-        """Budeme kooperovat, ať se děje co se děje."""
+        """Budeme kooperovat, ať se děje co se děje.
+		
+		We cooperate no matter what.
+		"""
         return Move.cooperate
 
     def reward(self, result):
-        """Jak to dopadlo nás nezajímá."""
+        """Jak to dopadlo nás nezajímá.
+		
+		We don't care what the result was.
+		"""
         pass
--- always_deceive.py
+++ always_deceive.py
@@ -1,16 +1,32 @@
-from player import Player
-from player import Move
+from player import Move, Player
 
 
 class AlwaysDeceive(Player):
+    """Hráč vždy klamající (strategie D).
+	
+	Always-deceiving player (strategy D).
+	"""
+	
     def __init__(self):
+        """Nepotřebujeme si nic pamatovat.
+		
+		We don't need to store anything.
+		"""
         pass
 
     def author_name(self):
         return "Martin"
 
     def next_move(self):
+        """Budeme klamat, ať se děje co se děje.
+		
+		We deceive no matter what.
+		"""
         return Move.deceive
 
     def reward(self, result):
+        """Jak to dopadlo nás nezajímá.
+		
+		We don't care what the result was.
+		"""
         pass
